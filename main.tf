a9bb0d89105c2c70eff8372b1025b7b2222522d2
aadurajaram <rajaram.aadavan@infoservices.com>
2026-02-24 16:27:20 +0530

    Add Terraform main configuration

---
+++ main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mc_poc_bucket" {
+  bucket = "mc-terrateam-poc-aadurajaram-2026"
+}
